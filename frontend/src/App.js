a08fbf94e2d3f5dc17c8809a78740ed74786c094
MrHidey <navasns0409@gmail.com>
2025-11-14 21:53:27 +0530

    Basic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 
 function App() {
   const issues=[
-    {id:1,title:"UI Bug",owner:"Navas",status:"Open"},
-    {id:2,title:"Login Error",owner:"Sana",status:"Closed"},
-    {id:3,title:"Login Error",owner:"Abhi",status:"Closed"},
-    {id:4,title:"Login Error",owner:"Rafi",status:"Open"},
-    {id:5,title:"Login Error",owner:"Preethi",status:"Closed"}
+    {id:1,title:"UI Bug",owner:"Navas",status:"Open",createdAt:"2025-11-10",effort:2,dueDate:"2025-11-12"},
+    {id:2,title:"Login Error",owner:"Sana",status:"Closed",createdAt:"2025-11-14",effort:1,dueDate:"2025-11-15"},
+    {id:3,title:"Data Base Error",owner:"Abhi",status:"Closed",createdAt:"2025-11-21",effort:2,dueDate:"2025-11-23"},
+    {id:4,title:"Login Error",owner:"Rafi",status:"Open",createdAt:"2025-11-10",effort:4,dueDate:"2025-11-14"},
+    {id:5,title:"Not Responsive",owner:"Preethi",status:"Closed",createdAt:"2025-11-10",effort:1,dueDate:"2025-11-11"}
   ]
 
   return (
@@ -18,6 +18,9 @@ function App() {
           <th className='border border-gray-300 px-4 py-2'>Title</th>
           <th className='border border-gray-300 px-4 py-2'>Owner</th>
           <th className='border border-gray-300 px-4 py-2'>Status</th>
+          <th className='border border-gray-300 px-4 py-2'>CreatedAt</th>
+          <th className='border border-gray-300 px-4 py-2'>Effort</th>
+          <th className='border border-gray-300 px-4 py-2'>Due Date</th>
         </tr>
       </thead>
       <tbody>
@@ -27,6 +30,9 @@ function App() {
             <td className='border border-gray-300 px-4 py-2'>{i.title}</td>
             <td className='border border-gray-300 px-4 py-2'>{i.owner}</td>
             <td className='border border-gray-300 px-4 py-2'>{i.status}</td>
+            <td className='border border-gray-300 px-4 py-2'>{i.createdAt}</td>
+            <td className='border border-gray-300 px-4 py-2'>{i.effort}</td>
+            <td className='border border-gray-300 px-4 py-2'>{i.dueDate}</td>
           </tr>
         ))}
       </tbody>
